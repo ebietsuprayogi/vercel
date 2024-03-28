ec0e049106a73264886effcd57444a41bbcbe331
Ebiet Suprayogi <ebietsy@gmail.com>
2024-03-29 04:10:53 +0700

    new edit

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -13,7 +13,7 @@ import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
+import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
 import profilepic from '../images/profilepic.jpg';
 import {
   About,
@@ -202,31 +202,31 @@ export const portfolioItems: PortfolioItem[] = [
     title: 'HIMASISTIFO Website',
     description: 'The project involves the development of a website for the Information Systems Student Association of Politeknik Caltex Riau. This website serves as an informational platform for the association, providing details about its activities and upcoming events. It includes features such as committee registration and member profiles. Each member, including the governors, has a dedicated profile page where they can input information such as upcoming activities, documentation, and committee registration details. The governors are designated as super administrators, granting them special privileges for managing the website. This website aims to streamline communication within the association, enhance member engagement, and centralize the management of association activities and initiatives.',
     url: '#',
-    image: porfolioImage2,
+    image: porfolioImage4,
   },
   {
     title: 'E-Registrasi',
     description: 'The E-Registration system for Bank Riau Kepri serves as a comprehensive solution for recording the attendance of all bank employees using QR code technology. Additionally, it allows the addition of new activities and sends notifications to employees via email. The system is integrated with an API that enables connectivity to the mobile application, making the website an integral part of the overall application ecosystem. Through this system, the bank aims to streamline attendance tracking, facilitate communication with employees through email notifications, and provide a seamless experience across web and mobile platforms.',
     url: '#',
-    image: porfolioImage2,
+    image: porfolioImage4,
   },
   {
     title: 'SIAKMS',
     description: 'The Academic Information and Student Monitoring System, based on a website platform, serves as my final project aimed at providing comprehensive information to students, teachers, administrative staff, and the student body. Students have access to view their grades, provide comments, check any disciplinary actions taken against them, and register for extracurricular activities. Teachers can input grades, oversee extracurricular activities, assign disciplinary actions, and schedule meetings with parents or guardians of students as needed. Administrative staff are responsible for managing users, types of student achievements, and extracurricular data, including assigning teachers as supervisors. The student body can access all extracurricular and achievement data. This system aims to enhance communication and transparency within the educational institution, streamline administrative processes, and empower stakeholders with valuable insights into student performance and activities.',
     url: '#',
-    image: porfolioImage2,
+    image: porfolioImage4,
   },
   {
     title: 'Aspirasi',
     description: 'The Aspiration project aims to provide a platform for students and the surrounding community of Politeknik Caltex Riau to voice their aspirations, provide information, and lodge complaints. This application features multiple user levels, with the initial user being able to submit aspirations, information, or complaints to Politeknik Caltex Riau. These submissions are then reviewed by administrators. If accepted, they are escalated to the relevant executive level. Users receive email notifications regarding the status of their reports. This system fosters transparency, accountability, and engagement between the institution and its stakeholders, ultimately contributing to a more responsive and inclusive environment.',
     url: '#',
-    image: porfolioImage2,
+    image: porfolioImage4,
   },
   {
     title: 'Edu Kids(Android & Website',
     description: 'Edukids is a program designed to facilitate the learning process for children through various multimedia formats, including audio, video, and written materials. The system incorporates interactive exercises utilizing images to engage children in learning activities, ensuring an enjoyable and stimulating learning experience. Additionally, the program offers a consultation feature, allowing parents to easily communicate with teachers or seek advice regarding their childs education. This comprehensive approach not only supports childrens learning but also provides parents with satisfaction and peace of mind knowing they can actively participate in their childs educational journey.',
     url: '#',
-    image: porfolioImage2,
+    image: porfolioImage4,
   },
 ];
 
