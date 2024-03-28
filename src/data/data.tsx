1fa65a0afbc1ce5d8acb094a94c3e29f6e6aa582
Ebiet Suprayogi <ebietsy@gmail.com>
2024-03-29 03:52:08 +0700

    new edit

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -13,11 +13,6 @@ import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
-import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
-import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
-import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
-import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
 import profilepic from '../images/profilepic.jpg';
 import {
   About,
@@ -214,7 +209,7 @@ export const portfolioItems: PortfolioItem[] = [
     title: 'HIMASISTIFO Website',
     description: 'The project involves the development of a website for the Information Systems Student Association of Politeknik Caltex Riau. This website serves as an informational platform for the association, providing details about its activities and upcoming events. It includes features such as committee registration and member profiles. Each member, including the governors, has a dedicated profile page where they can input information such as upcoming activities, documentation, and committee registration details. The governors are designated as super administrators, granting them special privileges for managing the website. This website aims to streamline communication within the association, enhance member engagement, and centralize the management of association activities and initiatives.',
     url: '#',
-    image: porfolioImage1,
+    image: porfolioImage2,
   },
   {
     title: 'E-Registrasi',
@@ -226,19 +221,19 @@ export const portfolioItems: PortfolioItem[] = [
     title: 'SIAKMS',
     description: 'The Academic Information and Student Monitoring System, based on a website platform, serves as my final project aimed at providing comprehensive information to students, teachers, administrative staff, and the student body. Students have access to view their grades, provide comments, check any disciplinary actions taken against them, and register for extracurricular activities. Teachers can input grades, oversee extracurricular activities, assign disciplinary actions, and schedule meetings with parents or guardians of students as needed. Administrative staff are responsible for managing users, types of student achievements, and extracurricular data, including assigning teachers as supervisors. The student body can access all extracurricular and achievement data. This system aims to enhance communication and transparency within the educational institution, streamline administrative processes, and empower stakeholders with valuable insights into student performance and activities.',
     url: '#',
-    image: porfolioImage3,
+    image: porfolioImage2,
   },
   {
     title: 'Aspirasi',
     description: 'The Aspiration project aims to provide a platform for students and the surrounding community of Politeknik Caltex Riau to voice their aspirations, provide information, and lodge complaints. This application features multiple user levels, with the initial user being able to submit aspirations, information, or complaints to Politeknik Caltex Riau. These submissions are then reviewed by administrators. If accepted, they are escalated to the relevant executive level. Users receive email notifications regarding the status of their reports. This system fosters transparency, accountability, and engagement between the institution and its stakeholders, ultimately contributing to a more responsive and inclusive environment.',
     url: '#',
-    image: porfolioImage4,
+    image: porfolioImage2,
   },
   {
     title: 'Edu Kids(Android & Website',
     description: 'Edukids is a program designed to facilitate the learning process for children through various multimedia formats, including audio, video, and written materials. The system incorporates interactive exercises utilizing images to engage children in learning activities, ensuring an enjoyable and stimulating learning experience. Additionally, the program offers a consultation feature, allowing parents to easily communicate with teachers or seek advice regarding their childs education. This comprehensive approach not only supports childrens learning but also provides parents with satisfaction and peace of mind knowing they can actively participate in their childs educational journey.',
     url: '#',
-    image: porfolioImage5,
+    image: porfolioImage2,
   },
 ];
 
@@ -294,7 +289,7 @@ export const experience: TimelineItem[] = [
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: 'Here is how you can reach me.',
   items: [
     {
       type: ContactType.Location,
@@ -320,6 +315,6 @@ export const contact: ContactSection = {
 export const socialLinks: Social[] = [
   {label: 'Github', Icon: GithubIcon, href: 'https://github.com/ebietsuprayogi'},
   {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/ebietsy'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/ebietsy/'},
   {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/ebiet_sy'},
 ];
